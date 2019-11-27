5c439339b38d83673b3a52390854d37ff543ae4b
Carlos Flores <carlos_4_92@hotmail.com>
2019-11-26 22:01:02 -0500

    se realizaron las operaciones basicas en el codigo de visual

--- MyFirstGame/Program.cs
+++ MyFirstGame/Program.cs
@@ -7,6 +7,31 @@ using System.Threading.Tasks;
 namespace MyFirstGame {
     class Program {
         static void Main (string[] args) {
+
+            int x = 5;
+            float y = 4.5f;
+            string str = "lorem ipsum";
+            bool myBool = false;
+
+            float result;
+
+            result = 5 + 7;
+            Console.WriteLine ("suma " + result);
+
+            result = 5 - 7;
+            Console.WriteLine ("resta " + result);
+
+            result = 5 * 7;
+            Console.WriteLine ("multiplicacion " + result);
+
+            result = 5f / 7f;
+            Console.WriteLine ("division " + result);
+
+            result = 5 % 7;
+            Console.WriteLine ("diferencia " + result);
+
+            Console.ReadLine ();
+
         }
     }
 }
